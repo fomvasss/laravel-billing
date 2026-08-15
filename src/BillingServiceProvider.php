cb45ce446ca7755767320a3aa48f30d65ae9f89e
Vas <fomvasss@gmail.com>
2026-08-15 14:01:26 +0300

    add minimal service provider stub

---
+++ src/BillingServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fomvasss\Billing;
+
+use Illuminate\Support\ServiceProvider;
+
+class BillingServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
